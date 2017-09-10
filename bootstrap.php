9a4aea15e1b87e94ef27b5565850afe4f65c0e1e
Fwolf <fwolf.aide+git@gmail.com>
2017-09-10 11:27:37 +0800

    New bootstrap and PHPUnit config to run tests

---
+++ bootstrap.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Bootstrap
+ *
+ *  - Register ClassLoader
+ *  - Load default and user config
+ *
+ * @copyright   Copyright 2017 Fwolf
+ * @license     https://opensource.org/licenses/MIT MIT
+ */
+
+$classLoader = require __DIR__ . '/vendor/autoload.php';
